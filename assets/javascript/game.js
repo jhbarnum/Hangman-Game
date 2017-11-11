dd3a4efb75a06ffdb60821ce8bf8ee140ec64934
Jared <Jared@Littles-MacBook-Pro.local>
2017-11-10 23:49:41 -0500

    functioning Hangmann

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 
-
+var newArr = [];
 var underscore = [];
 var word;
 var guessArr = [];
@@ -7,6 +7,7 @@ var encodedWord = [];
 var wrongGuess = "Bad Choices";
 var wins = 0;
 var checkWord;
+var yourGuessesArr = [];
 		// Array of random words for game
     var hangmanWordsArr = ['cheeseburger', 'computer', 'bicycle', 'riverfront', 'stereo', 'guitar', 'mountain', 'firestarter', 'ewok', 'minivan', 'argentina', 'snowboard', 'javascript', 'lightning', 'eggnog' ];
 
@@ -17,29 +18,20 @@ var checkWord;
       confirm("Do you want to play a game?");
 		 	//reset page for game start
 		 	var numberOfGuesses = 15;
+      //resest encodedWord to correct array length at the start of each play
       encodedWord = [];
+      //reset html elements at the start of each play
       document.getElementById("guesses").innerHTML = "";
       document.getElementById("wrongGuess").innerHTML = "";
       document.getElementById("remainingGuesses").innerHTML = numberOfGuesses;
+      //sets word to guess at random from array
       word = hangmanWordsArr[Math.floor(Math.random() * hangmanWordsArr.length)];
-      console.log(word);
-		 	 //convert word to undscores
+      //console.log(word);
+		 	 //call functionn to convert word to undscores
        convertWord (word);
        // capture keystroke guesses
        document.onkeyup = function(event) {
-        var letterGuess = String.fromCharCode(event.keyCode).toLowerCase();
-        //console.log('Event Char Code ' + event.keyCode);
-        //console.log('Letter from Char code ' + letterGuess);
-        
-
-
-
-  		// Array for placement of guesses
-      underscore = [word];
-  		//console.log("My Word" + underscore);
- // turn underscore[] to [ _,_,_,_,_,_,_,_,_]
- 
-
+       var letterGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
  // Check guess array against hangmanWordarr array 	
           for (i = 0; i < word.length; i++) {
@@ -48,24 +40,23 @@ var checkWord;
               guessArr = letterGuess;
               var removed = encodedWord.splice(i, 1, word[i]);
               document.getElementById('answer').innerHTML = encodedWord;
-
- // reveal correct guesses
- 				      //console.log(word.indexOf(i));// ALWAYS RETURNNING -1 ?
               document.getElementById("guesses").innerHTML = letterGuess;
-      
-              document.getElementById("wrongGuess").innerHTML = "Good";
-              encodedWordStr();
-                                                                              //break;
+              
+              encodedWordStr();                                                               
               }
               else 	{ 
                  document.getElementById("guesses").innerHTML = letterGuess;
-                 document.getElementById("wrongGuess").innerHTML = "Try Again";
+                 //document.getElementById("wrongGuess").innerHTML = "Try Again";
                    }		
                }
+            // displays guesses in new array
+            yourGuessesArr.push(letterGuess);
+            document.getElementById("wrongGuess").innerHTML = yourGuessesArr;
+
+
         		//# of guesses remaining
            numberOfGuesses = numberOfGuesses - 1;
            document.getElementById("remainingGuesses").innerHTML = numberOfGuesses;
-
            if (numberOfGuesses <= 0){
              document.getElementById("wrongGuess").innerHTML = "Game Over";
     				//start over
@@ -74,29 +65,29 @@ var checkWord;
             }
           if (checkWord == word) {
             console.log("piaz");
-            wins++;
+            
             document.getElementById('gameWin').innerHTML = wins;
             confirm("YOU WIN!");
             beginGame();
-            }
-              //console.log(encodedWord);
-                //console.log(word);
+            //add 1 to win count if game is won.
+            wins++;   
+            }   
           }
           function convertWord (word) {
-
               for (var i = 0; i < word.length; i++) {
                // word[i]
                var newStr = word.charAt(i);
                //console.log(newStr);
                encodedWord.push("_");
                document.getElementById('answer').innerHTML = encodedWord;
-               //console.log(encodedWord);{}
                   }
                 }
+                //turns array into str for comparison
           function encodedWordStr() {
               checkWord = encodedWord.join('');
               console.log(checkWord);
               }
+         
         }
            
 	        	
